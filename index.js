9d9a2d7de42edd7f408d7333ef3fc7b2686f1dab
thibauts <thibaut.seguy@gmail.com>
2014-06-05 07:49:51 +0200

    basic bus client

---
+++ index.js
@@ -0,0 +1,127 @@
+var EventEmitter  = require('events').EventEmitter;
+var util          = require('util');
+var tls           = require('tls');
+var debug         = require('debug')('castv2-message-bus');
+var protocol      = require('./protocol.js');
+var PacketStreamWrapper = require('./packet-stream-wrapper');
+
+var CastMessage = protocol.CastMessage;
+
+function MessageBus() {
+  EventEmitter.call(this);
+  this.socket = null;
+  this.ps = null;
+}
+
+util.inherits(MessageBus, EventEmitter);
+
+MessageBus.prototype.connect = function(options, callback) {
+  var self = this;
+
+  if(typeof options === 'string') {
+    options = {
+      host: options
+    };
+  }
+
+  options.port = options.port || 8009;
+  options.rejectUnauthorized = false;
+
+  if(callback) this.once('connect', callback);
+
+  debug('connecting to %s:%d ...', options.host, options.port);
+
+  this.socket = tls.connect(options, function() {
+    self.ps = new PacketStreamWrapper(self.socket);
+    self.ps.on('packet', onpacket);
+
+    debug('connected');
+    self.emit('connect');
+  });
+
+  this.socket.on('error', onerror);
+  this.socket.once('close', onclose);
+
+  function onerror(err) {
+    debug('error: %s %j', err.message, err);
+    self.emit('error', err);
+  }
+
+  function onclose() {
+    debug('connection closed');
+    self.ps.removeListener('packet', onpacket);
+    self.socket.removeListener('error', onerror);
+    self.socket = null;
+    self.ps = null;
+    self.emit('close');
+  }
+
+  function onpacket(buf) {
+    var message = CastMessage.parse(buf);
+
+    debug(
+      'recv message: protocolVersion=%s sourceId=%s destinationId=%s namespace=%s data=%s',
+      message.protocolVersion,
+      message.sourceId,
+      message.destinationId,
+      message.namespace,
+      message.payloadType === 'BINARY' 
+        ? util.inspect(message.payloadBinary)
+        : message.payloadUtf8
+    );    
+
+    if(message.protocolVersion !== 'CASTV2_1_0') {
+      self.emit('error', new Error('Unsupported protocol version: ' + message.protocolVersion));
+      self.close();
+      return;
+    }
+
+    self.emit('message',
+      message.sourceId,
+      message.destinationId,
+      message.namespace,
+      message.payloadType === 'BINARY' 
+        ? message.payloadBinary 
+        : message.payloadUtf8
+    );    
+  }
+
+};
+
+MessageBus.prototype.close = function() {
+  debug('closing connection ...');
+  this.socket.end();
+};
+
+MessageBus.prototype.send = function(sourceId, destinationId, namespace, data) {
+  var message = {
+    protocolVersion: 0, // CASTV2_1_0
+    sourceId: sourceId,
+    destinationId: destinationId,
+    namespace: namespace
+  };
+
+  if(Buffer.isBuffer(data)) {
+    message.payloadType = 1 // BINARY;
+    message.payloadBinary = data;
+  } else {
+    message.payloadType = 0 // STRING;
+    message.payloadUtf8 = data;
+  }
+
+  debug(
+    'send message: protocolVersion=%s sourceId=%s destinationId=%s namespace=%s data=%s',
+    message.protocolVersion,
+    message.sourceId,
+    message.destinationId,
+    message.namespace,
+    message.payloadType === 1 // BINARY
+      ? util.inspect(message.payloadBinary)
+      : message.payloadUtf8
+  );
+
+  var buf = CastMessage.serialize(message);
+  this.ps.send(buf);
+};
+
+module.exports = MessageBus;
---
+++ packet-stream-wrapper.js
@@ -0,0 +1,44 @@
+var EventEmitter  = require('events').EventEmitter;
+var inherits      = require('util').inherits;
+
+var WAITING_HEADER  = 0;
+var WAITING_PACKET = 1;
+
+function PacketStreamWrapper(stream) {
+  EventEmitter.call(this);
+
+  this.stream = stream;
+
+  var state = WAITING_HEADER;
+  var packetLength = 0;
+
+  var self = this;
+  this.stream.on('readable', function() {
+    switch(state) {
+      case WAITING_HEADER:
+        var header = stream.read(4);
+        if(header !== null) {
+          packetLength = header.readUInt32BE(0);
+          state = WAITING_PACKET;
+        }
+        break;
+      case WAITING_PACKET:
+        var packet = stream.read(packetLength);
+        if(packet !== null) {
+          self.emit('packet', packet);
+          state = WAITING_HEADER;
+        }
+        break;
+    }
+  });
+}
+
+inherits(PacketStreamWrapper, EventEmitter);
+
+PacketStreamWrapper.prototype.send = function(buf) {
+  var header = new Buffer(4);
+  header.writeUInt32BE(buf.length, 0);
+  this.stream.write(Buffer.concat([header, buf]));
+};
+
+module.exports = PacketStreamWrapper;
