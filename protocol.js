0022304ac7d43c693777ffdd01009d0e6c69954d
thibauts <thibaut.seguy@gmail.com>
2014-06-05 05:29:26 +0200

    protocol

---
+++ protocol.js
@@ -0,0 +1,19 @@
+var fs        = require('fs');
+var protobuf  = require('protobuf');
+
+var desc = fs.readFileSync('cast_channel.desc');
+var schema = new protobuf.Schema(desc);
+
+var package_ = 'extensions.api.cast_channel';
+
+var messages = [
+  'CastMessage', 
+  'AuthChallenge', 
+  'AuthResponse', 
+  'AuthError', 
+  'DeviceAuthMessage'
+];
+
+messages.forEach(function(message) {
+  module.exports[message] = schema[package_ + '.' + message];
+});
